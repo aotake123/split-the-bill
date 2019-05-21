9f587de36a2da26174e8523b324af19359f4c1d6
Tasuku Ohki <tasukuoki3@gmail.com>
2019-05-21 15:35:28 +0900

    ヘッダー内項目変更

--- head.php
+++ head.php
@@ -3,5 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>割り勘シェアハウス</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
   </head>
   <body>
--- header.php
+++ header.php
@@ -1,35 +1,50 @@
   <header>
+     
       <div class="header_top">
-        <nav>
-          <ul>
-            <li>新規登録（無料）</li>
-            <li>ログイン</li>
+        <nav class="h_t_nav">
+          <ul class="h_t_nav_list">
+          <?php
+           if(empty($_SESSION['user_id'])){
+            ?>
+            <li class="h_t_nav_list_item"><a href=index.php>新規登録（無料）</a></li>
+            <li class="h_t_nav_list_item"><a href="login.php">ログイン</a></li>
+            <?php
+           }else{
+            ?>
+            <li class="h_t_nav_list_item"><a href=mypage.php>マイページ</a></li>
+            <li class="h_t_nav_list_item"><a href="logout.php">ログアウト</a></li>
+            <?php        
+           }
+            ?>
           </ul>
         </nav>
       </div>
+      
       <div class="header_sub">
         <div class="h_logo_wrap">
           <div class="h_logo_top">
-            <img src="" alt="">
-            <h1 class="">割り勘シェアハウス</h1>
+            <img src="images/header_logo.jpg" class="h_logo_jpg" alt="割り勘シェアハウス">
+            <h1 class="title">割り勘シェアハウス</h1>
           </div>
           <div class="h_logo_bottom">
-            あなたの家の、割り勘の記録・管理をお手伝い
+            <p>あなたの家の、割り勘の記録・管理をお手伝い</p>
           </div>
         </div>
       </div>
+      
       <div class="header_last">
+       <div class="header_last_wrap">
         <div class="h_l_tab">
-          <ul>
-            <li>トップ</li>
-            <li>割り勘の申請</li>
-            <li>割り勘の支払い</li>
-            <li>割り勘の一覧</li>
-            <li>割り勘の確認</li>
+          <ul class="h_l_tab_ul">
+            <li class="h_l_tab_li"><a href="index.php">トップ</a></li>
+            <li class="h_l_tab_li"><a href="payEditSeparate.php">割り勘の申請</a></li>
+            <li class="h_l_tab_li"><a href="payEditReturn.php">割り勘の支払い</a></li>
+            <li class="h_l_tab_li"><a href="payList.php">割り勘の一覧</a></li>
+            <li class="h_l_tab_li"><a href="profEdit.php">プロフィール編集</a></li>
           </ul>
         </div>
-        <div class="h_l_right">
-          割り勘シェアハウスへのご意見をお聞かせください
+        
         </div>
       </div>
+      
     </header>
