fd2ff0128b056f6dc8195a07f25030f74eb84f74
Tasuku Ohki <tasukuoki3@gmail.com>
2019-05-28 23:27:15 +0900

    SESSIONの時間に関する変数を修正

--- head.php
+++ head.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>割り勘シェアハウス</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="graph.php">
     <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
   </head>
   <body>
