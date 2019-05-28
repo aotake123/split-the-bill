ca7ed94f9011459257aa3fcefc4b4bd434571938
Tasuku Ohki <tasukuoki3@gmail.com>
2019-05-28 23:24:58 +0900

    グラフ関数追加

--- footer.php
+++ footer.php
@@ -1,5 +1,19 @@
     <footer>
-      Copyright AOTAKE. ALL RIGHT RESERVED.
+     <div class="footer_contents">
+      <p class="copywrite">Copyright AOTAKE. ALL RIGHT RESERVED.</p>
+      </div>
     </footer>
-  </body>
+    
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script>
+        $(function(){
+        //テキストエリアカウント
+          var $countUp = $('#js-count'),
+              $countView = $('#js-count-view');
+          $countUp.on('keyup', function(e){
+              $countView.html($(this).val().length);
+          });
+        }):
+    </script>
+  </body>  
 </html>
