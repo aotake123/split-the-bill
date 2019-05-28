4d72613bc7bf59117c84429531f66244b8ce76f0
Tasuku Ohki <tasukuoki3@gmail.com>
2019-05-28 23:31:44 +0900

    GETパラメータ準備、グラフ表示関連の関数整備

--- payEditSeparate.php
+++ payEditSeparate.php
@@ -16,7 +16,9 @@ require('auth.php');
 //==============================
 // 画面表示用データ取得
 //==============================
-//各割り勘を表示目的で判別する為のGETデータを格納
+//請求/支払いフラグ（0が請求,1が支払い）GETパラメータの有無で判別
+$isClaim = (!empty($_GET['isc_id'])) ? 1 : 0;
+//各割り勘詳細画面向けリンクを表示する為のGETデータを格納
 $s_id = (!empty($_GET['s_id'])) ? $_GET['s_id'] : '';
 //DBから割り勘固有データを取得
 $dbBillData = (!empty($s_id)) ? getSplitbill($_SESSION['user_id'], $s_id) : null;
@@ -28,12 +30,12 @@ $dbFormData = getUser($_SESSION['user_id']);
 $group_name = $dbFormData['group_name'];
 //ユーザーの所属するグループの「人数」を取得
 $dbMemberCount = getMemberCount($_SESSION['user_id'],$group_name);
-//ユーザーの所属するグループメンバーの情報を取得
+//ユーザーの所属するグループメンバー全員の情報を取得（ユーザーが先頭）
 $dbMemberData = getMemberdata($_SESSION['user_id'],$group_name);
 //新規登録画面か編集画面かの判別用フラグ
 $edit_flg = (empty($dbBillData)) ? false :true;
 
-//debug('割り勘ID：'.$s_id); //登録後に付けられるデータの為、開発前は保留
+//debug('割り勘ID：'.$s_id); //DB登録後に付けられるデータ
 //debug('割り勘固有のDBデータ：'.print_r($dbBillData,true)); //登録ができるようになったら解放
 //debug('割り勘項目データ：'.print_r($dbItemData,true)); //OK
 //debug('ユーザーの所属するグループの人数：'.print_r($dbMemberCount,true)); //OK
@@ -59,6 +61,9 @@ if(!empty($_POST)){
 	$totalCost = $_POST['totalCost'];
 	$comment = $_POST['comment'];
 	$users = $_SESSION['user_id'];
+	$g_year = $_POST['g_year'];
+	$g_month = $_POST['g_month'];
+	$g_date = $_POST['g_date'];
 
 	//中間テーブル照合用のIDを作成
 	$id2 = makerandkey();	//割り勘テーブル側に代入
@@ -68,10 +73,16 @@ if(!empty($_POST)){
 	for($i=0; $i<$dbMemberCount; ++$i){
 		$userCost[$i] = $_POST["userCost$i"];
 		$userIsClaim[$i] = $_POST["userIsClaim$i"];
+		debug('$_POST["userCost$i"]の中身：'.print_r($_POST["userCost$i"],true));
+		debug('$_POST["userIsClaim$i"]の中身：'.print_r($_POST["userIsClaim$i"],true));
+		//$userIsClaim[$i]がNULLでエラー判定になるのを回避する処理
+		if($userIsClaim[$i] === NULL){
+			$userIsClaim[$i] = 0;
+		}
+		if($userCost[$i] === NULL){
+			$userCost[$i] = 0;
+		}
 	}
-
-	//請求/支払いフラグ（0が請求,1が支払い）
-	$isClaim = 0;
 
 	//画像アップロード
 	$receipt = ( !empty($_FILES['receipt']['name']) ) ? uploadImg($_FILES['receipt'],'receipt') : '';
@@ -116,17 +127,20 @@ if(!empty($_POST)){
 					debug('DB更新です。');
 						$sql = 'UPDATE payment SET title = :title, item = :item, 
 						totalCost = :totalCost, isClaim = :isClaim, receipt = :receipt,
-						comment = :comment, users = :users, group_name = :group_name, id2 = :id2';
+						comment = :comment, users = :users, group_name = :group_name, id2 = :id2,
+						g_year = :g_year, g_month = :g_month, g_date = :g_date';
 						$data = array(':title' => $title, ':item'=> $item, ':totalCost' => $totalCost,
 						':isClaim' => $isClaim, ':receipt' => $receipt, ':comment' => $comment, 
-						':users' => $_SESSION['user_id'], ':group_name' => $group_name, ':id2' => $payment);
+						':users' => $_SESSION['user_id'], ':group_name' => $group_name, ':id2' => $payment,
+						':g_year' => $g_year, ':g_month' => $g_month, ':g_date' => $g_date);
 					}else{
 						debug('DB新規登録です。');
-						$sql = 'INSERT INTO payment (title,item,totalCost,isClaim,receipt,comment,users,group_name,id2,createDate) 
-						values (:title,:item,:totalCost,:isClaim,:receipt,:comment,:users,:group_name,:id2,:createDate)';
+						$sql = 'INSERT INTO payment (title,item,totalCost,isClaim,receipt,comment,users,group_name,id2,createDate,g_year,g_month,g_date) 
+						values (:title,:item,:totalCost,:isClaim,:receipt,:comment,:users,:group_name,:id2,:createDate,:g_year,:g_month,:g_date)';
 						$data = array(':title' => $title, ':item' => $item, ':totalCost' => $totalCost,
 						':isClaim' => $isClaim, ':receipt' => $receipt, ':comment' => $comment, 
-						':users' => $_SESSION['user_id'], ':group_name' => $group_name, ':id2' => $payment, ':createDate' => date('Y-m-d H:i:s'));
+						':users' => $_SESSION['user_id'], ':group_name' => $group_name, ':id2' => $payment, ':createDate' => date('Y-m-d H:i:s'),
+						':g_year' => $g_year, ':g_month' => $g_month, ':g_date' => $g_date);
 						}
 					debug('SQL：'.$sql);
 					debug('流し込みデータ：'.print_r($data,true));
@@ -191,7 +205,17 @@ require('header.php');
       <div class="main_1colum_wide2">
         <form class="form" action="" method="post" enctype="multipart/form-data">
            <div class="form_title_wrap">
-               <div class="form_title_subject line_blue"><h2>自分で買ったものをメンバーへ割り勘申請する</h2></div>
+				<?php
+				if(empty($_GET['isc_id'])) {
+				?>
+               <div class="form_title_subject line_blue"><h2>自分が支払った会計の割り勘を、メンバーに依頼する</h2></div>
+				<?php
+				}else{
+				?>
+					<div class="form_title_subject line_pink"><h2>メンバーが支払ったものに対して、自分の割り勘分を支払う</h2></div>
+				<?php
+				}
+				?>
            </div>
            <div class="form_main">
            <div class="form_main_wrap">
@@ -204,22 +228,79 @@ require('header.php');
             	<div class="prof_whole_left">
             		<div class="prof_whole_wrap">
             			<div class="p_w_l_form">割り勘タイトル</div>
-            			<div class="p_w_l_attention_off"></div>
+            			<div class="p_w_l_attention_on">必須</div>
             		</div>
             	</div>
             	<div class="prof_whole_right">
             		<div class="p_w_r_left">
-                <input type="text" name="title" value="<?php echo getFormData('title'); ?>">
+                <input type="text" name="title" value="<?php echo getBillFormData('title'); ?>">
             		</div>
             		<div class="p_w_r_right"></div>
             	</div>
 			</div>
+
+            <div class="prof_whole prof_whole_line">
+            	<div class="prof_whole_left">
+            		<div class="prof_whole_wrap">
+            			<div class="p_w_l_form">割り勘をした日</div>
+            			<div class="p_w_l_attention_on">必須</div>
+            		</div>
+            	</div>
+            	<div class="prof_whole_right">
+            		<div class="p_w_r_left">
+						<div>
+							 <select name="g_year">
+                                <option value="2019" <?php if(empty(getBillFormData('g_year'))) echo 'selected="selected"'; ?>>2019</option>
+                                <option value="2020" <?php if(getBillFormData('g_year') == 2020) echo 'selected="selected"'; ?>>2020</option>
+                                <option value="2021" <?php if(getBillFormData('g_year') == 2021) echo 'selected="selected"'; ?>>2021</option>
+                                <option value="2022" <?php if(getBillFormData('g_year') == 2022) echo 'selected="selected"'; ?>>2022</option>
+                                <option value="2023" <?php if(getBillFormData('g_year') == 2023) echo 'selected="selected"'; ?>>2023</option>
+                                <option value="2024" <?php if(getBillFormData('g_year') == 2024) echo 'selected="selected"'; ?>>2024</option>
+                            </select>
+						</div>
+						<div>年</div>
+						<div>
+							<select name="g_month">
+									<option value="0" <?php if(empty(getBillFormData('g_month'))) echo 'selected="selected"'; ?>>▶︎選択してください</option> 
+									<option value="1" <?php if(getBillFormData('g_month') == 1) echo 'selected="selected"'; ?>>1</option>
+									<option value="2" <?php if(getBillFormData('g_month') == 2) echo 'selected="selected"'; ?>>2</option>
+									<option value="3" <?php if(getBillFormData('g_month') == 3) echo 'selected="selected"'; ?>>3</option>
+									<option value="4" <?php if(getBillFormData('g_month') == 4) echo 'selected="selected"'; ?>>4</option>
+									<option value="5" <?php if(getBillFormData('g_month') == 5) echo 'selected="selected"'; ?>>5</option>
+									<option value="6" <?php if(getBillFormData('g_month') == 6) echo 'selected="selected"'; ?>>6</option>
+									<option value="7" <?php if(getBillFormData('g_month') == 7) echo 'selected="selected"'; ?>>7</option>
+									<option value="8" <?php if(getBillFormData('g_month') == 8) echo 'selected="selected"'; ?>>8</option>
+									<option value="9" <?php if(getBillFormData('g_month') == 9) echo 'selected="selected"'; ?>>9</option>
+									<option value="10" <?php if(getBillFormData('g_month') == 10) echo 'selected="selected"'; ?>>10</option>
+									<option value="11" <?php if(getBillFormData('g_month') == 11) echo 'selected="selected"'; ?>>11</option>
+									<option value="12" <?php if(getBillFormData('g_month') == 12) echo 'selected="selected"'; ?>>12</option>
+                            </select>
+						</div>
+						<div>月</div>
+						<div>
+                           <select name="g_date">
+                                <option value="0" <?php if(empty(getBillFormData('g_date'))) echo 'selected="selected"'; ?>>▶︎選択してください</option> 
+                                <?php
+                                for($i = 1; $i < 32; $i++){
+                                ?>
+                                    <option value="<?php echo $i; ?>" <?php if(getBillFormData('g_date') == $i) echo 'selected="selected"'; ?>><?php echo $i; ?></option>;
+                                <?php
+                                }
+                                ?>
+                            </select>
+						</div>
+						<div>日</div>
+            		</div>
+            		<div class="p_w_r_right"></div>
+            	</div>
+			</div>
+
            
             <div class="prof_whole prof_whole_line">
             	<div class="prof_whole_left">
             		<div class="prof_whole_wrap">
             			<div class="p_w_l_form">割り勘項目名</div>
-            			<div class="p_w_l_attention_off"></div>
+            			<div class="p_w_l_attention_on">必須</div>
             		</div>
             	</div>
             	<div class="prof_whole_right">
@@ -227,11 +308,11 @@ require('header.php');
 
 							<label class="<?php if(!empty($err_msg['item'])) echo 'err'; ?>">
 							 <select name="item">
-                                <option value="0" <?php if(empty(getFormData('item'))) echo 'selected="selected"'; ?>>▶︎選択してください</option>
+                                <option value="0" <?php if(empty(getBillFormData('item'))) echo 'selected="selected"'; ?>>▶︎選択してください</option>
                                 <?php
                                 foreach($dbItemData as $key => $val){
                                 ?>
-                                <option value="<?php echo $val['id']?>" <?php if(getFormData('item') == $val['id']) echo 'selected="selected"' ?>><?php echo $val['data'] ?></option>
+                                <option value="<?php echo $val['id']?>" <?php if(getBillFormData('item') == $val['id']) echo 'selected="selected"' ?>><?php echo $val['data'] ?></option>
                                 <?php echo $val['data']; ?>
                                 <?php
                                 }
@@ -248,7 +329,7 @@ require('header.php');
             	<div class="prof_whole_left">
             		<div class="prof_whole_wrap">
             			<div class="p_w_l_form">買い物の合計金額</div>
-            			<div class="p_w_l_attention_off"></div>
+            			<div class="p_w_l_attention_on">必須</div>
             		</div>
             	</div>
             	<div class="prof_whole_right">
@@ -264,46 +345,36 @@ require('header.php');
 			</div>
            
 
-            <div class="prof_whole">
-            	<div class="prof_whole_left">
-            		<div class="prof_whole_wrap">
-            			<div class="p_w_l_form">あなたが負担する金額</div>
-            			<div class="p_w_l_attention_off"></div>
-            		</div>
-            	</div>
-            	<div class="prof_whole_right">
-            		<div class="img_wrap">
-            			<div class="img_upload_left">
-            				<div><img src="<?php echo getFormData('pic'); ?>" alt="ログイン者の写真" class="img_prev"></div>
-							<div><?php echo getFormData('nickname'); ?></div>
-						</div>
-						<div class="img_upload_check">
-            				<input type="checkbox" name="" value="" class="pay_checkbox">
-            			</div>
-
-            			<div class="img_upload_right pay_separate_option">
-              			<div class="p_w_r_left_option1">
-               				<input type="text" name="userCost0" value="<?php if(!empty($_POST["userCost0"])) echo $_POST['userCost0']; ?>">
-							<input type="hidden" name="userIsClaim0" value="0">
-
-                		</div>
-                		<div class="p_w_r_left_option2">円
-						</div>
-
-            			</div>
-            		</div>
-            		<div class="p_w_r_right"></div>
-            	</div>
-			</div>
 
 			<?php
                 foreach($dbMemberData as $key => $val):
             ?>
 
+			<?php
+                if($key === 0 && $isClaim === 1){
+				}else{
+            ?>
+
+
 			 <div class="prof_whole">
             	<div class="prof_whole_left">
             		<div class="prof_whole_wrap">
-            			<div class="p_w_l_form"><?php if($key == 0 ){ echo '割り勘を請求する<br />相手と金額'; } ?><!-- タイトル無し --></div>
+            			<div class="p_w_l_form">
+
+						<?php
+						if($key === 0){
+							echo 'あなたが負担する金額'; 
+						}else if($key === 1){
+							if(empty($_GET['isc_id'])){
+								echo '割り勘を請求する<br />相手と金額'; 
+						   }else if(!empty($_GET['isc_id'])){
+								echo '支払う金を渡す<br />相手と金額'; 
+						   }
+						}
+						?>
+
+						<!-- タイトル無し -->
+						</div>
             			<div class="p_w_l_attention_off"></div>
             		</div>
             	</div>
@@ -315,12 +386,20 @@ require('header.php');
 							<div><?php echo $val['nickname']; ?></div>
 						</div>
            				<div class="img_upload_check">
-            				<input type="checkbox" name="" value="" class="pay_checkbox">
+						   <!-- JSで入力補助機能を付けた際に使うチェックボックス -->
+            				<!-- <input type="checkbox" name="" value="" class="pay_checkbox"> -->
             			</div>
             			<div class="img_upload_right pay_separate_option">
               			<div class="p_w_r_left_option1">
-               				<input type="text" name="userCost<?php echo $key+1; ?>" value="<?php if(!empty($_POST["userCost$key+1"])) echo $_POST["userCost$key+1"]; ?>">
-							<input type="hidden" name="userIsClaim<?php echo $key+1; ?>" value="1">
+               				<input type="text" name="userCost<?php echo $key; ?>" value="<?php if(!empty($_POST["userCost$key"])) echo $_POST["userCost$key"]; ?>">
+							<input type="hidden" name="userIsClaim<?php echo $key; ?>"
+							 value="<?php
+							 if($key === 0){
+								if(empty($_GET['isc_id'])) { echo 0; }else{ echo 1; }; 
+							}else{
+								if(empty($_GET['isc_id'])) { echo 1; }else{ echo 0; }; 
+							}
+							 ?>">
 
                 		</div>
                 		<div class="p_w_r_left_option2">円
@@ -329,10 +408,11 @@ require('header.php');
             			</div>
             		</div>
             		<div class="p_w_r_right"></div>
-            	</div>
+				</div>
 
 			<?php 
-				debug('情報配列のキー取得確認：'.print_r($key,true));
+				}
+				//debug('情報配列のキー取得確認：'.print_r($key,true));
 			?>
 
 
@@ -350,13 +430,13 @@ require('header.php');
             	<div class="prof_whole_right">
             		<div class="img_wrap">
             			<div class="img_upload_left">
-            				<img src="<?php echo getFormData('receipt'); ?>" alt="profile" class="img_prev">
+            				<img src="<?php echo getBillFormData('receipt'); ?>" alt="profile" class="img_prev">
 						</div>
             			<div class="img_upload_right">
 							<div class="img_upload_btn">
          						<!-- アップロードしたい画像を選択 -->
           						<input type="hidden" name="MAX_FILE_SIZE" size="3145728">
-								<input type="file" name="receipt" value="<?php echo getFormData('receipt'); ?>">
+								<input type="file" name="receipt" value="<?php echo getBillFormData('receipt'); ?>">
           					</div>
            					<p class="img_comment">※イメージ画像・写真を設定できます</p>
             			</div>
@@ -374,7 +454,7 @@ require('header.php');
             	</div>
             	<div class="prof_whole_right">
             		<div class="p_w_r_left">
-						<textarea name="comment" rows="6" cols="36" class="pay_comment"><?php echo getFormData('comment');?></textarea>
+						<textarea name="comment" rows="6" cols="36" class="pay_comment"><?php echo getBillFormData('comment');?></textarea>
             		</div>
             		<div class="p_w_r_right"></div>
             	</div>
